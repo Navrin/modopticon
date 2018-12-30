b73a6b75aa2211549ccf18b3b8f1e243a577138e
Sam Rose <samwho@lbak.co.uk>
2018-12-30 16:48:15 +0000

    Fixed up various warnings.

--- src/main/java/uk/co/samwho/modopticon/storage/InvalidResourceIdentifierException.java
+++ src/main/java/uk/co/samwho/modopticon/storage/InvalidResourceIdentifierException.java
@@ -1,6 +1,8 @@
 package uk.co.samwho.modopticon.storage;
 
 public final class InvalidResourceIdentifierException extends Exception {
+  private static final long serialVersionUID = 1165696686023325700L;
+
   private final String resourceIdentifier;
 
   public InvalidResourceIdentifierException(String resourceIdentifier) {
